36e8d390d0db00e68384d69d7fe7a9f49936e372
Raul Ospina <raminka13@hotmail.com>
2024-03-27 14:11:41 -0500

    New: FAQs data

---
+++ src/data/ftfFAQs.js
@@ -0,0 +1,51 @@
+export const ftfFAQs = [
+  {
+    title: "What funding is available from FTF?",
+    content:
+      "Initial approvals are typically available for" +
+      "private or private + instrument training. Additional loan" +
+      "funding for advanced training is often available to" +
+      "students with a solid payment history, successful training" +
+      "progress, and the ability to support an increased debt and" +
+      "payment amount. There is not a fixed cap in the amount that" +
+      "can be loaned.",
+  },
+  {
+    title: "Are rates variable or fixed?",
+    content:
+      "With Flight Training Finance, rates are fixed." +
+      "FTF offers an option to earn back some of your interest" +
+      "charge with on-time payments. See the FTF website for" +
+      "details.",
+  },
+  {
+    title: "Is deferred repayment available? When does repayment begin?",
+    content:
+      "No deferred payment is available. Payments" +
+      "begin 30 days after the training plan is executed. This" +
+      "means the borrower and/or their co-borrower must have a" +
+      "means to repay training while it is occurring.",
+  },
+  {
+    title: "Do I need a co-signer?",
+    content:
+      "Those with limited credit experience or income often will need a co-signer.",
+  },
+  {
+    title: "What are the repayment terms?",
+    content:
+      "Payment terms are directly tied to the" +
+      "frequency of your training. You will commit to a maximum" +
+      "number of lessons per week based on the payment term you" +
+      "select. The longer your repayment term, the lower the" +
+      "number of lessons per week your training plan will allow.",
+  },
+  {
+    title: "How does my training get funded?",
+    content:
+      "Students will sign 'Training Reports' to be" +
+      "submitted by Summit Flight for each lesson provided in" +
+      "order for your Summit Flight Academy account to be credited" +
+      "for charges incurred.",
+  },
+];
---
+++ src/data/stratusFAQs.js
@@ -0,0 +1,50 @@
+export const stratusFAQs = [
+  {
+    title: "What funding is available from Stratus Financing?",
+    content:
+      "Stratus offers everything from full career" +
+      "track financing to financing for your next certificate or" +
+      "rating. Packages can include everything you need from" +
+      "supplies, checkride fees, etc.",
+  },
+  {
+    title: "Are rates variable or fixed?",
+    content:
+      "Stratus offers a variety of financing options" +
+      "to meet your needs. When conditionally approved, you'll be" +
+      "offered multiple options.",
+  },
+  {
+    title: "Is deferred payment available? When does repayment begin?",
+    content:
+      "One of the best benefits of Stratus Financing" +
+      "is the ability to defer payments while in training, if" +
+      "desired. Stratus also offers the ability to pay interest" +
+      "only while building your time to ATP minimums.",
+  },
+  {
+    title: "Do I need a co-signer?",
+    content:
+      "Possibly. If you don't have assets to cover" +
+      "your loan and/or employment to cover the payments, often a" +
+      "co-signer will make it possible for Stratus to approve a" +
+      "loan for you. Once you apply, Stratus will review your" +
+      "application and advise of the need for a co-signer.",
+  },
+  {
+    title: "What are the repayment terms?",
+    content:
+      "Stratus offers a variety of repayment terms." +
+      "They will be offered to you upon conditional approval." +
+      "Options include various repayment schedules, fixed or" +
+      "variable rates, etc.",
+  },
+  {
+    title: "How does my training get funded?",
+    content:
+      "Stratus works directly with partner flight" +
+      "schools to provide funding directly into your training" +
+      "account. Flight schools agree to return unused funds in" +
+      "order to minimize your debt obligation.",
+  },
+];
