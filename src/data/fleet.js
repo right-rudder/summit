440a65da3caf234a2a750fed15c43bc580f1753a
Raul Ospina <raminka13@hotmail.com>
2024-04-06 15:05:01 -0500

    Update: all planes

--- src/data/fleet.js
+++ src/data/fleet.js
@@ -82,7 +82,7 @@ export const fleet = [
       },
     ],
     alt: "piper-warrior-at-summit-flight-academy",
-    desc: `Piper PA-28 Archer N4353L is a true gem in Summit Flight Academy's training fleet, offering the most utility of the fixed-gear PA-28 line and making it an ideal platform for instrument and commercial pilot training. This beautiful aircraft shines with its highly capable IFR panel, equipped with dual KX 155 NAV/COM radios, a Garmin GNX 375, and a G5 HSI, soon to be upgraded in February 2024 to a state-of-the-art G3X Touch 10.6" touchscreen glass panel, GTN 650, and GFC-500 Autopilot, transforming it into a cutting-edge Technically Advanced Aircraft (TAA). Beyond its impressive avionics, N4353L boasts an exquisite upgraded leather interior, providing a comfortable and stylish cabin environment perfect for cross-country adventures, ensuring an exceptional training experience as you hone your instrument skills or build experience for a commercial certificate.`,
+    desc: `N425CB is one of Summit Flight Academy's latest fleet additions, a true standout with its comprehensive IFR panel, digital attitude indicator, updated paint scheme, and brand-new interior. This aircraft boasts an impressive avionics suite, including an AV-30c digital attitude indicator, a Garmin 430W, a Garmin audio panel with a second com radio, and an ADS-B in/out transponder, ensuring seamless communication and navigation capabilities. The stunning new interior of N425CB not only looks sharp but also provides a comfortable and modern environment for your training flights. With its impressive instrumentation, updated aesthetics, and top-notch amenities, N425CB provides a cutting-edge and enjoyable flight training experience.`,
     rentalPrice: 160,
     model: "PA-28-151",
     flightRules: "VFR + IFR  (Day and Night)",
@@ -90,7 +90,7 @@ export const fleet = [
       "Garmin 430W, AV-30c Digital Attitude Indicator,  ADS-B in/out, second nav and comm.",
     specifications: {
       exterior: {
-        exteriorHeight: "7 ft 3.6 in (2.225 m)",
+        exteriorHeight: "7 ft 3.6 in",
         wingSpan: "35 ft",
         length: "23 ft 8 in",
       },
@@ -109,7 +109,7 @@ export const fleet = [
         serviceCeiling: "12,700 ft",
       },
       distances: {
-        takeoffDistance: "1065 ft",
+        takeoffDistance: "1,065 ft",
         landingDistance: "595 ft",
       },
       performance: {
@@ -125,5 +125,260 @@ export const fleet = [
         horsepower: "150 hp",
       },
     },
+  },
+  {
+    id: 2,
+    tailNumber: "N15283",
+    name: "Piper Cherokee",
+    images: [
+      {
+        src: "/img/our-fleet/N15283-1.webp",
+        alt: "piper-cherokee-at-summit-flight-academy",
+      },
+      {
+        src: "/img/our-fleet/N15283-2.webp",
+        alt: "piper-cherokee-interior-control-panel",
+      },
+      {
+        src: "/img/our-fleet/N15283-3.webp",
+        alt: "piper-cherokee-interior-seats",
+      },
+    ],
+    alt: "piper-cherokee-at-summit-flight-academy",
+    desc: `N15283, the flagship aircraft of Summit Flight, has played an integral role in training numerous aspiring pilots. Boasting an IFR-capable instrument panel equipped with dual Garmin G5 ADI/HSI instruments, along with a cutting-edge Garmin avionics stack featuring a GTN 650Xi and GTX 345 ADS-B in/out transponder, N15283 ensures a state-of-the-art learning experience for students. Recently, this aircraft underwent significant upgrades, including the installation of all-new windows and a luxurious leather interior, enhancing both comfort and style for those on board.`,
+    rentalPrice: 160,
+    model: "PA-28-140",
+    flightRules: "VFR + IFR  (Day and Night)",
+    avionics: "Standard w/ ADS-B in/out",
+    specifications: {
+      exterior: {
+        exteriorHeight: "7 ft 3.6 in",
+        wingSpan: "30 ft 0 in",
+        length: "23 ft 3.6 in",
+      },
+      occupancy: {
+        crew: 1,
+        pax: 3,
+      },
+      weights: {
+        maxTakeoffWeight: "2,150 lb",
+        emptyWeight: "1,201 lb",
+        fuelCapacity: "50 gal",
+        usefulPayload: "1,134 lb",
+        fullFuelPayload: "637 lb",
+      },
+      range: {
+        maxRange: "465 nm",
+        serviceCeiling: "14,300 ft",
+      },
+      distances: {
+        takeoffDistance: "870 ft",
+        landingDistance: "925 ft",
+      },
+      performance: {
+        maxSpeed: "123 kts",
+        normalCruiseSpeed: "108 kts",
+        rateOfClimb: "660 fpm",
+      },
+      powerplant: {
+        engines: 1,
+        engineManufacturer: "Lycoming",
+        engineModel:
+          "O-320-E2A 4-cylinder air-cooled horizontally-opposed piston engine",
+        horsepower: "160 hp",
+      },
+    },
+  },
+  {
+    id: 3,
+    tailNumber: "N15044",
+    name: "Piper Cherokee",
+    images: [
+      {
+        src: "/img/our-fleet/N15044-1.webp",
+        alt: "piper-cherokee-at-summit-flight-academy",
+      },
+      {
+        src: "/img/our-fleet/N15044-2.webp",
+        alt: "piper-cherokee-interior-control-panel",
+      },
+      {
+        src: "/img/our-fleet/N15044-3.webp",
+        alt: "piper-cherokee-interior-seats",
+      },
+    ],
+    alt: "piper-cherokee-at-summit-flight-academy",
+    desc: `The Piper PA-28 series, including the Cherokee, Warrior, and Archer is one of most popular single engine aircraft ever made. It is still in production today and is a favorite for flight training thanks to its safety record, good performance, and ease of operation.`,
+    rentalPrice: 160,
+    model: "PA-28-140",
+    flightRules: "VFR + IFR",
+    avionics:
+      "Garmin 345 Audio Panel, GTN 650Xi Nav/Comm/GPS, Garmin GTX 345 ADS-B in/out transponder and Garmin 660 VFR GPS",
+    specifications: {
+      exterior: {
+        exteriorHeight: "7 ft 3.6 in",
+        wingSpan: "30 ft 0 in",
+        length: "23 ft 3.6 in",
+      },
+      occupancy: {
+        crew: 1,
+        pax: 3,
+      },
+      weights: {
+        maxTakeoffWeight: "2,150 lb",
+        emptyWeight: "1,201 lb",
+        fuelCapacity: "50 gal",
+        usefulPayload: "1,134 lb",
+        fullFuelPayload: "637 lb",
+      },
+      range: {
+        maxRange: "465 nm",
+        serviceCeiling: "14,300 ft",
+      },
+      distances: {
+        takeoffDistance: "870 ft",
+        landingDistance: "925 ft",
+      },
+      performance: {
+        maxSpeed: "123 kts",
+        normalCruiseSpeed: "108 kts",
+        rateOfClimb: "660 fpm",
+      },
+      powerplant: {
+        engines: 1,
+        engineManufacturer: "Lycoming",
+        engineModel:
+          "O-320-E2A 4-cylinder air-cooled horizontally-opposed piston engine",
+        horsepower: "150 hp",
+      },
+    },
+  },
+  {
+    id: 4,
+    tailNumber: "N6894W",
+    name: "Piper Cherokee",
+    images: [
+      {
+        src: "/img/our-fleet/N6894W-1.webp",
+        alt: "piper-cherokee-at-summit-flight-academy",
+      },
+      {
+        src: "/img/our-fleet/N6894W-2.webp",
+        alt: "piper-cherokee-interior-control-panel",
+      },
+      {
+        src: "/img/our-fleet/N6894W-3.webp",
+        alt: "piper-cherokee-interior-seats",
+      },
+    ],
+    alt: "piper-cherokee-at-summit-flight-academy",
+    desc: `The Piper PA-28 series, including the Cherokee, Warrior, and Archer is one of most popular single engine aircraft ever made. It is still in production today and is a favorite for flight training thanks to its safety record, good performance, and ease of operation.`,
+    rentalPrice: 160,
+    model: "PA-28-140",
+    flightRules: "VFR",
+    avionics:
+      "PMA-8000 Audio panel, KLX-135A Comm/GPS, Garmin GTX 345 ADS-B in/out transponder and Garmin 660 VFR GPS",
+    specifications: {
+      exterior: {
+        exteriorHeight: "7 ft 3.6 in",
+        wingSpan: "30 ft 0 in",
+        length: "23 ft 3.6 in",
+      },
+      occupancy: {
+        crew: 1,
+        pax: 3,
+      },
+      weights: {
+        maxTakeoffWeight: "2,150 lb",
+        emptyWeight: "1,201 lb",
+        fuelCapacity: "50 gal",
+        usefulPayload: "1,134 lb",
+        fullFuelPayload: "637 lb",
+      },
+      range: {
+        maxRange: "465 nm",
+        serviceCeiling: "14,300 ft",
+      },
+      distances: {
+        takeoffDistance: "870 ft",
+        landingDistance: "925 ft",
+      },
+      performance: {
+        maxSpeed: "123 kts",
+        normalCruiseSpeed: "108 kts",
+        rateOfClimb: "660 fpm",
+      },
+      powerplant: {
+        engines: 1,
+        engineManufacturer: "Lycoming",
+        engineModel:
+          "O-320-E2A 4-cylinder air-cooled horizontally-opposed piston engine",
+        horsepower: "150 hp",
+      },
+    },
+  },
+  {
+    id: 5,
+    tailNumber: "N7664Y",
+    name: "Piper Twin Commanche",
+    images: [
+      {
+        src: "/img/our-fleet/N7664Y-1.webp",
+        alt: "piper-twin-commanche-at-summit-flight-academy",
+      },
+      {
+        src: "/img/our-fleet/N7664Y-2.webp",
+        alt: "piper-twin-commanche-at-summit-flight-academy",
+      },
+      {
+        src: "/img/our-fleet/N7664Y-3.webp",
+        alt: "piper-twin-commanche-at-summit-flight-academy",
+      },
+    ],
+    alt: "piper-twin-commanche-at-summit-flight-academy",
+    desc: `Stay tuned for the arrival of N7664Y, a Piper Twin Comanche aircraft, set to join our fleet in Spring 2024. This addition promises advanced features and capabilities, allowing us to conduct more advanced training like our Multi-Engine Rating.`,
+    model: "Coming Spring 2024!",
+    /* rentalPrice: 160, */
+    /* flightRules: "VFR", */
+    /* avionics:
+      "PMA-8000 Audio panel, KLX-135A Comm/GPS, Garmin GTX 345 ADS-B in/out transponder and Garmin 660 VFR GPS", */
+    /* specifications: {
+      exterior: {
+        exteriorHeight: "7 ft 3.6 in",
+        wingSpan: "30 ft 0 in",
+        length: "23 ft 3.6 in",
+      },
+      occupancy: {
+        crew: 1,
+        pax: 3,
+      },
+      weights: {
+        maxTakeoffWeight: "2,150 lb",
+        emptyWeight: "1,201 lb",
+        fuelCapacity: "50 gal",
+        usefulPayload: "1,134 lb",
+        fullFuelPayload: "637 lb",
+      },
+      range: {
+        maxRange: "465 nm",
+        serviceCeiling: "14,300 ft",
+      },
+      distances: {
+        takeoffDistance: "870 ft",
+        landingDistance: "925 ft",
+      },
+      performance: {
+        maxSpeed: "123 kts",
+        normalCruiseSpeed: "108 kts",
+        rateOfClimb: "660 fpm",
+      },
+      powerplant: {
+        engines: 1,
+        engineManufacturer: "Lycoming",
+        engineModel:
+          "O-320-E2A 4-cylinder air-cooled horizontally-opposed piston engine",
+        horsepower: "150 hp",
+      },
+    }, */
   },
 ];
