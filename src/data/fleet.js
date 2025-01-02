8daaadc1fd423b927cc15dde39943f55cfaf751b
Tim Jedrek <tim@timjedrek.com>
2025-01-02 16:28:36 -0600

    update price..

--- src/data/fleet.js
+++ src/data/fleet.js
@@ -19,7 +19,7 @@ export const fleet = [
     ],
     alt: "piper-archer-ii-at-summit-flight-academy",
     desc: `Piper PA-28 Archer N4353L is a true gem in Summit Flight Academy's training fleet, offering the most utility of the fixed-gear PA-28 line and making it an ideal platform for instrument and commercial pilot training. This beautiful aircraft shines with its highly capable IFR panel and boasts an upgraded leather interior, providing a comfortable and stylish cabin environment perfect for cross-country adventures, ensuring an exceptional training experience as you hone your instrument skills or build experience for a commercial certificate.`,
-    rentalPrice: 175,
+    rentalPrice: 180,
     model: "PA-28-181",
     flightRules: "VFR/IFR",
     avionics: "Dual NAV/COM, IFR GPS, ADS-B IN/OUT",
@@ -83,7 +83,7 @@ export const fleet = [
     ],
     alt: "piper-warrior-at-summit-flight-academy",
     desc: `N425CB is one of Summit Flight Academy's latest fleet additions and a true standout with its IFR panel, digital attitude indicator, updated paint scheme, and brand-new interior. This aircraft boasts an impressive avionics suite and a stunning new interior. With its impressive instrumentation, updated aesthetics, and top-notch amenities, N425CB provides a cutting-edge and enjoyable flight training experience.`,
-    rentalPrice: 160,
+    rentalPrice: 165,
     model: "PA-28-151",
     flightRules: "VFR + IFR  (Day and Night)",
     avionics:
@@ -146,7 +146,7 @@ export const fleet = [
     ],
     alt: "piper-cherokee-at-summit-flight-academy",
     desc: `N15283, the flagship aircraft of Summit Flight, has played an integral role in training numerous aspiring pilots. Boasting an IFR-capable instrument panel equipped with dual Garmin G5 ADI/HSI instruments, along with a cutting-edge Garmin avionics stack featuring a GTN 650Xi and GTX 345 ADS-B in/out transponder, N15283 ensures a state-of-the-art learning experience for students. Recently, this aircraft underwent significant upgrades, including the installation of all-new windows and a luxurious leather interior, enhancing both comfort and style for those on board.`,
-    rentalPrice: 160,
+    rentalPrice: 165,
     model: "PA-28-140",
     flightRules: "VFR + IFR  (Day and Night)",
     avionics: "Standard w/ ADS-B in/out",
@@ -209,7 +209,7 @@ export const fleet = [
     ],
     alt: "piper-cherokee-at-summit-flight-academy",
     desc: `The Piper PA-28 series, including the Cherokee, Warrior, and Archer is one of most popular single engine aircraft ever made. It is still in production today and is a favorite for flight training thanks to its safety record, good performance, and ease of operation.`,
-    rentalPrice: 160,
+    rentalPrice: 165,
     model: "PA-28-140",
     flightRules: "VFR + IFR",
     avionics:
@@ -273,7 +273,7 @@ export const fleet = [
     ],
     alt: "piper-cherokee-at-summit-flight-academy",
     desc: `"Miss Whiskey," a beloved aircraft among our clientele aspiring for private pilot certification, is none other than N6894W. This aircraft is equipped with a highly functional VFR panel featuring ADS-B in/out capabilities, accompanied by a Garmin 660 GPS navigator that provides traffic and ADS-B weather display for enhanced situational awareness. Moreover, N6894W boasts an updated interior, ensuring a comfortable and modern flying experience. Additionally, the inclusion of four-point inertial reel seat belts up front further enhances safety measures for passengers and pilots alike.`,
-    rentalPrice: 150,
+    rentalPrice: 155,
     model: "PA-28-140",
     flightRules: "VFR",
     avionics:
@@ -338,7 +338,7 @@ export const fleet = [
     alt: "piper-twin-comanche-at-summit-flight-academy",
     desc: "Summit Flight Academy proudly introduces the newest addition to our fleet, the Piper Twin Comanche (PA-30), used specifically for Multi-Engine Training. This versatile and reliable aircraft is renowned for its exceptional performance, making it the perfect choice for aspiring multi-engine pilots.",
     model: "Dual instructor only (Solo rental unavailable)",
-    rentalPrice: 295,
+    rentalPrice: 300,
     additionalCost: "Instruction Rate: +$80/hour",
     addSubtitle1: "Training Advantages:",
     addText1:
